5e614a4fc8f0d75a0ecfaf52471dffd153d46c5f
MAWAAW <16020356+MAWAAW@users.noreply.github.com>
2024-10-09 08:49:46 +0200

    First Commit

---
+++ src/main/java/org/mawaaw/springbootportadapterarchitecture/SpringBootPortAdapterArchitectureApplication.java
@@ -0,0 +1,13 @@
+package org.mawaaw.springbootportadapterarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPortAdapterArchitectureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootPortAdapterArchitectureApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/mawaaw/springbootportadapterarchitecture/SpringBootPortAdapterArchitectureApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mawaaw.springbootportadapterarchitecture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootPortAdapterArchitectureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
